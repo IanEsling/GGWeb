28edc8447b451ba24d674a61a0adf4bd630bb636
Ian <ian.esling@gmail.com>
2012-11-14 20:51:05 +0000

    run job on start up

--- grails-app/jobs/geegees/GetRacesForTodayJob.groovy
+++ grails-app/jobs/geegees/GetRacesForTodayJob.groovy
@@ -7,8 +7,8 @@ class GetRacesForTodayJob {
     RacingPostRaceService racingPostRaceService
 
     static triggers = {
-        cron name: 'getRaces', cronExpression: "0 5 9 * * ?"
-//        simple name: 'getRaces', startDelay: 60000, repeatInterval: 600000
+//        cron name: 'getRaces', cronExpression: "0 5 9 * * ?"
+        simple name: 'getRaces', startDelay: 60000, repeatInterval: 600000
     }
 
     def execute() {
